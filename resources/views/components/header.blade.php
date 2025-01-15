e77d2d9d6286378a05f5bd7ddbf87d00c96464bf
Daniele Macrì <daniele.macr96@gmail.com>
2025-01-15 18:43:24 +0100

    rimane da modificare il carosello

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,59 @@
+<header>
+    <div class="container">
+        <div class="row ">
+
+
+            <div class="col-12 col-md-12 d-flex justify-content-center ul-social">
+                <i class="fa-brands fa-facebook fa-xl mx-2" style="color: #b0b0b0;"></i>
+                <i class="fa-brands fa-instagram fa-xl mx-2" style="color: #b0b0b0;"></i>
+                <i class="fa-brands fa-linkedin fa-xl mx-2" style="color: #b0b0b0;"></i>
+            </div>
+            <div class="col-12 col-md-12 d-flex flex-column justify-content-center">
+                <h3 class="display-3 text-center">Chiama per un appuntamento</h3>
+                <p class="display-4 mt-4 text-center"><i class="fa-solid fa-phone fa-2l" style="color: #dbdbdb;"></i>
+                    +39 333 953 2077</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row row-header">
+            <div class="col-12 col-md-5">
+                <h2 class="display-1 text-center mt-5">Chi sono</h2>
+                <p class="m-5">Mi chiamo Claudia Rumbo e sono Logopedista. Mi occupo di disturbi del linguaggio e
+                    della comunicazione, nonché di altre problematiche di interesse logopedico, che insorgono in età
+                    evolutiva, adulta e geriatrica. Lavoro privatamente presso il mio studio professionale. Inoltre,
+                    offro collaborazioni con asili, scuole dell’infanzia e scuole primarie, oltre ad altre istituzioni e
+                    professioni sanitari</p>
+            </div>
+
+            <div id="carouselExample"
+                class="carousel carousel-custom slide my-5 px-0 col-12 col-md-6 d-flex justify-content-end"
+                data-bs-slide="true">
+                <div class="carousel-inner" data-bs-slide="true">
+                    <div class="carousel-item active" data-bs-slide="true">
+                        <img src="/image/foto1.jpeg" class="d-block w-100" alt="immagine logopedista">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/image/foto2.jpeg" class="d-block w-100" alt="immagine logopedista">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/image/foto3.avif" class="d-block w-100" alt="immagine logopedista">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+    </div>
+
+</header>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg index-2">
+    <div class="container-fluid navbarcollaps">
+      <a class="navbar-brand" href="#"><img class="logo" src="/image/logo1.png" alt="logo sito"></a>
+      <button class="navbar-toggler buttonCollaps" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa-solid fa-bars" style="color: #f0ea2b;"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-link">
+            <a class="nav-link active text-white icon-navbar" aria-current="page" href="#"><i class="fa-solid fa-location-dot" style="color: #f0ea2b;"></i></a>
+          </li>
+          <li class="nav-link">
+            <a class="nav-link active text-white" aria-current="page" href="https://www.google.com/maps/dir//Via+Pietro+Mascagni,+21A,+00043+Ciampino+RM/@41.8090941,12.5132516,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x132589a1eda23059:0x3baec52e232d6b50!2m2!1d12.5956524!2d41.8091235?hl=it-IT&entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D">Via Pietro Mascagni 21/A, 00043, Ciampino</a>
+          </li>
+          <li class="nav-link">
+            <a class="nav-link active text-white" aria-current="page" href="#"><i class="fa-regular fa-envelope" style="color: #f0ea2b;"></i></a>
+          </li>
+          <li class="nav-link ">
+            <a class="nav-link text-white" href="#">Claudia.Rumbo@gmail.com</a>
+          </li>
+          {{-- <li class="nav-link dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li> --}}
+          
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
+
