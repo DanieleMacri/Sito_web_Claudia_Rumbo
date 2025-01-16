46d03f675f709cde8a5e1f0449b28776f812daca
Daniele Macrì <daniele.macr96@gmail.com>
2025-01-16 18:16:59 +0100

    finito i caroselli

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,14 +2,19 @@
     <div class="container">
         <div class="row ">
 
+            <div class="col-12 d-flex justify-content-center">
+                <h1 class="my-4">Claudia Rumbo</h1>
+            </div>
+
 
             <div class="col-12 col-md-12 d-flex justify-content-center ul-social">
                 <i class="fa-brands fa-facebook fa-xl mx-2" style="color: #b0b0b0;"></i>
                 <i class="fa-brands fa-instagram fa-xl mx-2" style="color: #b0b0b0;"></i>
-                <i class="fa-brands fa-linkedin fa-xl mx-2" style="color: #b0b0b0;"></i>
+                <a href="https://www.linkedin.com/in/claudia-rumbo-b60304201/"><i class="fa-brands fa-linkedin fa-xl mx-2" style="color: #b0b0b0;"></i></a>
+                
             </div>
             <div class="col-12 col-md-12 d-flex flex-column justify-content-center">
-                <h3 class="display-3 text-center">Chiama per un appuntamento</h3>
+                <h3 class=" text-center">Chiama per un appuntamento</h3>
                 <p class="display-4 mt-4 text-center"><i class="fa-solid fa-phone fa-2l" style="color: #dbdbdb;"></i>
                     +39 333 953 2077</p>
             </div>
@@ -18,7 +23,8 @@
     </div>
     <div class="container-fluid">
         <div class="row row-header">
-            <div class="col-12 col-md-5">
+            <div class="col-12 col-md-6 mt-5 my-5" data-aos="fade-up"
+            data-aos-duration="3000">
                 <h2 class="display-1 text-center mt-5">Chi sono</h2>
                 <p class="m-5">Mi chiamo Claudia Rumbo e sono Logopedista. Mi occupo di disturbi del linguaggio e
                     della comunicazione, nonché di altre problematiche di interesse logopedico, che insorgono in età
@@ -27,9 +33,48 @@
                     professioni sanitari</p>
             </div>
 
-            <div id="carouselExample"
-                class="carousel carousel-custom carousel-height slide my-5 px-0 col-12 col-md-6 d-flex justify-content-end"
-                data-bs-slide="true">
+            <div id="carouselExampleCaptions" class="carousel slide carousel carousel-custom carousel-height slide my-5 col-12 col-md-6 d-flex justify-content-end">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="/image/foto1.jpeg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/image/foto2.jpeg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/image/foto3.avif" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+
+            {{-- <div id="carouselExampleAutoplaying"
+                class="carousel carousel-custom carousel-height slide my-5 col-12 col-md-6 d-flex justify-content-end"
+                data-bs-slide="true" data-aos="fade-up"
+                data-aos-duration="3000"> 
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  </div>
                 <div class="carousel-inner" data-bs-slide="true">
                     <div class="carousel-item active" data-bs-slide="true">
                         <img src="/image/foto1.jpeg" class="d-block w-100" alt="immagine logopedista">
@@ -51,9 +96,49 @@
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
-            </div>
+            </div> --}}
 
         </div>
     </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+                <div class="" data-aos="fade-up" data-aos-duration="3000">
+                    <h2 class="display-1 text-center mt-5">Il mio studio</h2>
+                    <p class="m-5 text-center">Situato nella zona di Ciampino in Via Pietro Mascagni 21/A</a>, lo studio offre un ambiente tranquillo e confortevole che coniuga la buona riuscita del trattamento con la serenità della persona.</p>
+                </div>
+            </div>
 
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 d-flex justify-content-center">
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="/image/foto3.avif" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/image/foto3.avif" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="/image/foto3.avif" class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+        </div>
+    </div>
 </header>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,21 +1,37 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
     
-    <x-navbar/>
-    
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Modak&family=Playwrite+VN&family=Roboto+Condensed:ital@0;1&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <x-navbar />
+
     <div class="min-vh-100">
         {{ $slot }}
     </div>
-    <x-footer/>
+    <x-footer />
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://kit.fontawesome.com/a8290e7443.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
